4d80291653a1f577099fe4636d9efc3c3bc64a11
ywindows98 <124464870+ywindows98@users.noreply.github.com>
2024-02-18 14:08:04 +0100

    Implement the display of entities on the page
    Iterated objects of the Model.

--- TDL_ASP/Controllers/ToDoTaskController.cs
+++ TDL_ASP/Controllers/ToDoTaskController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TDL_ASP.Data;
+using TDL_ASP.Models;
 
 namespace TDL_ASP.Controllers
 {
     public class ToDoTaskController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ToDoTaskController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<ToDoTask> objToDoTaskList = _db.Tasks;
+            return View(objToDoTaskList);
         }
     }
 }
